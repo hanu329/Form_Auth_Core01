ca61089258a773918ca1ba80d893bf7cdb2ab0e2
hanu <thanuman222@gmail.com>
2024-01-26 18:45:32 +0530

    interfc

---
+++ Interfaces/IAuthServices.cs
@@ -0,0 +1,12 @@
+﻿using FormAuthCore.Models;
+
+namespace FormAuthCore.Interfaces
+{
+    public interface IAuthServices
+    {
+        User AddUser(User user);
+        string Login(LoginRequest loginRequest);
+        Role AddRole(Role role);
+        bool AssignRoleToUser(AddUserRole obj);
+    }
+}
